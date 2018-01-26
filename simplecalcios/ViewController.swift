49562520eb86a8f3c6cc86c0c6beb1bbaa638bdc
Kevin Tran <ktran@tack.tech>
2018-01-25 16:12:36 -0800

    add functionality for number press

--- simplecalcios/ViewController.swift
+++ simplecalcios/ViewController.swift
@@ -9,99 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var expressionString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        display.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        let buttonTitle = sender.title(for: .normal)
+        
+        switch buttonTitle {
+        case "0"?, "1"?, "2"?, "3"?, "4"?, "5"?, "6"?, "7"?, "8"?, "9"?:
+            display.text = display.text! + buttonTitle!
+            expressionString += buttonTitle!
+            
+        case "+"?:
+            break
+        case "-"?:
+            break
+        case "*"?:
+            break
+        case "/"?:
+            break
+        case "%"?:
+            break
+        case "ct"?:
+            break
+        case "avg"?:
+            break
+        case "fact"?:
+            break
+        case "="?:
+            break
+        default:
+            break
+        }
+    }
 
-}
-
-class Calculator {
-    
-    public func add(lhs: Int, rhs: Int) -> Int {
-        return lhs + rhs
-    }
-    
-    public func add(_ args:[Int]) -> Int {
-        let size = args.count - 1
-        var sum = 0
-        for num in 0...size {
-            sum += args[num]
-        }
-        return sum
-    }
-    
-    public func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
-        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
-    }
-    
-    public func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
-        let x = lhs["x"]! + rhs["x"]!
-        let y = lhs["y"]! + rhs["y"]!
-        return ["x": x, "y": y]
-    }
-    
-    public func subtract(lhs: Int, rhs: Int) -> Int {
-        return lhs - rhs
-    }
-    
-    public func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
-        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
-    }
-    
-    public func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
-        let x = lhs["x"]! - rhs["x"]!
-        let y = lhs["y"]! - rhs["y"]!
-        return ["x": x, "y": y]
-    }
-    
-    public func multiply(lhs: Int, rhs: Int) -> Int {
-        return lhs * rhs
-    }
-    
-    public func multiply(_ args:[Int]) -> Int {
-        let size = args.count - 1
-        var sum = 1
-        for num in 0...size {
-            sum *= args[num]
-        }
-        return sum
-    }
-    
-    public func divide(lhs: Int, rhs: Int) -> Int {
-        return lhs / rhs
-    }
-    
-    public func count(_ args:[Int]) -> Int {
-        return args.count
-    }
-    
-    public func avg(_ args:[Int]) -> Int{
-        let size = args.count
-        var sum = 0
-        for num in 0...size - 1 {
-            sum += args[num]
-        }
-        return sum / size
-    }
-    
-    public func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
-        return op(lhs, rhs)
-    }
-    
-    public func mathOp(args:[Int], beg: Int, op: (Int, Int) -> Int) -> Int {
-        var sum = beg
-        let size = args.count - 1
-        for num in 0...size {
-            sum = op(sum, args[num])
-        }
-        return sum
-    }
 }
 
